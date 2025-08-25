f388d80aa0583b5bdf51d47bfb9adc63206006ef
abrahamramirez2k03 <119706581+abrahamramirez2k03@users.noreply.github.com>
2025-08-25 18:23:08 -0500

    Update programac31.py

--- programac31.py
+++ programac31.py
@@ -5,3 +5,7 @@ st.title('Titulo del proyecto')
 st.write('12345')
 click=st.button('click')
 activated=st.toggle("activar")
+number=st.slider("Elige un número", 0, 3600)
+df = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+columns=['lat', 'lon'])
