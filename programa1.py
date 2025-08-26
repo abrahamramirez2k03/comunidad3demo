bc814ee66f4d4f5a65e6aa6f85aa1533c56d6d0a
abrahamramirez2k03 <119706581+abrahamramirez2k03@users.noreply.github.com>
2025-08-25 19:09:23 -0500

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -2,3 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Titulo del proyecto')
+st.write('12345')
+click=st.button('click')
+activated=st.toggle("activar")
+number=st.slider("Elige un número", 0, 3600)
+st.write("El número es: ",number) 
+n = st.slider("n", 5,100, step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
+st.line_chart(chart_data)
+
+choice=st.number_input("Elige un número: ", 1, 31)
